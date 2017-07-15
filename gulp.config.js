7025a9f9c59fcc8342e232f17b003ff07c52b7e0
Gagan <g.deepsingh1@gmail.com>
2017-07-15 16:50:36 +0530

    comp

--- gulp.config.js
+++ gulp.config.js
@@ -1,9 +1,12 @@
 module.exports = function() {
+    var clients = './src/client/';
     var config = {
+        temp: './.tmp',
         alljs: [
             './src/**/*.js',
             './*.js'
-        ]
+        ],
+        less: clients + 'styles/styles.less'
     };
     return config;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var args = require('yargs').argv;
 var $ = require('gulp-load-plugins')({lazy: true});
+var del = require('del');
 var config = require('./gulp.config')();
 
 gulp.task('pass-joins', function() {
@@ -10,3 +11,26 @@ gulp.task('pass-joins', function() {
         .pipe($.jshint())
         .pipe($.jshint.reporter('jshint-stylish', {verbose: true}));
 });
+
+gulp.task('styles', ['clean-styles'], function() {
+    return gulp.src(config.less)
+          .pipe($.plumber())
+          .pipe($.less())
+          //.pipe($.autoprefixer({browsers: ['last 2 version', '> 5%']}))
+          .pipe(gulp.dest(config.temp));
+});
+
+
+gulp.task('clean-styles', function() {
+    var files = config.temp + '**/*.css';
+    clean(files)
+});
+
+gulp.task('less-watcher', function() {
+    gulp.watch([config.less], ['styles']);
+});
+
+
+function clean(path) {
+    del(path);
+}
